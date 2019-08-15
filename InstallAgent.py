044eeca451a8a351e0f627a6f5c6f080140295b9
tom <tom@demayfamly.net>
2019-08-15 09:49:52 -0600

    Replace string source of plist file with structure.
    Use plistlib to write file form structure.  This allows easier modification of the plist file.
    Add option to specify arbitrary launchctl tags to  plist.
    Environment variables now specified by command line option.
    Improved target file kind determination.

--- InstallAgent.py
+++ InstallAgent.py
@@ -6,22 +6,33 @@
 #  3) launch the agent
 #
 ###############
-
-import os
-import argparse
-import sys
-import time
-import configparser
-import logging
-import logging.config
-import logging.handlers
-import json
-from getpass import getuser as getuser
-import shutil
+### Python3 Library documentation   #   https://docs.python.org/3/library
+import time             #   https://docs.python.org/3/library/time.html
+import datetime         #   https://docs.python.org/3/library/datetime.html
+from datetime import date           #   https://docs.python.org/3/library/datetime.html#date-objects
+from datetime import timedelta      #   https://docs.python.org/3/library/datetime.html#timedelta-objects
+from datetime import datetime as dt #   https://docs.python.org/3/library/datetime.html#datetime-objects
+from datetime import time as dtime  #   https://docs.python.org/3/library/datetime.html#time-objects
+import os               #   https://docs.python.org/3/library/os.html
+import sys              #   https://docs.python.org/3/library/sys.html
+import argparse         #   https://docs.python.org/3/library/argparse.html
+import configparser     #   https://docs.python.org/3/library/configparser.html
+import logging          #   https://docs.python.org/3/library/logging.html
+import logging.config   #   https://docs.python.org/3/library/logging.config.html
+import logging.handlers #   https://docs.python.org/3/library/logging.handlers.html
+import json             #   https://docs.python.org/3/library/json.html
+import shlex            #   https://docs.python.org/3/library/shlex.html
+import re               #   https://docs.python.org/3/library/re.html
+import shutil           #   https://docs.python.org/3/library/shutil.html
+from getpass import getuser #   https://docs.python.org/3/library/getpass.html
+import plistlib as PL   #   https://docs.python.org/3/library/plistlib.html
 
 ProgName, ext = os.path.splitext(os.path.basename(sys.argv[0]))
 ProgPath = os.path.dirname(os.path.realpath(sys.argv[0]))
+#####  Setup logging; first try for file specific, and if it doesn't exist, use a folder setup file.
 logConfFileName = os.path.join(ProgPath, ProgName + '_loggingconf.json')
+if not os.path.isfile(logConfFileName):
+    logConfFileName = os.path.join(ProgPath, 'Loggingconf.json')
 if os.path.isfile(logConfFileName):
     try:
         with open(logConfFileName, 'r') as logging_configuration_file:
@@ -33,12 +44,22 @@ if os.path.isfile(logConfFileName):
             logPath=""
         for p in config_dict['handlers'].keys():
             if 'filename' in config_dict['handlers'][p]:
-                config_dict['handlers'][p]['filename'] = os.path.join(logPath, config_dict['handlers'][p]['filename'])
+                fn = os.path.join(logPath, config_dict['handlers'][p]['filename'].replace('<replaceMe>', ProgName))
+                config_dict['handlers'][p]['filename'] = fn
+                # print('Setting handler %s filename to: %s'%(p, fn))
+
+        # # program specific logging configurations:
+        config_dict["handlers"]["debugconsole"]["level"] = 'NOTSET'
+        # # config_dict["handlers"]["debug_file_handler"]["class"] = 'logging.FileHandler'
+        # config_dict["handlers"]["debug_file_handler"]["mode"] = '\'w\''
+
         logging.config.dictConfig(config_dict)
     except Exception as e:
         print("loading logger config from file failed.")
         print(e)
         pass
+else:
+    print("Logging configuration file not found.")
 
 logger = logging.getLogger(__name__)
 logger.info('logger name is: "%s"', logger.name)
@@ -68,71 +89,75 @@ knownExecutables = {
   <key>GroupName</key>
   <string>{groupName}</string>
 '''
-plistTemplate = '''<?xml version="1.0" encoding="UTF-8"?>
-<!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-<plist version="1.0">
-<dict>
-  <key>Label</key>
-  <string>{myDomain}.{progName}</string>
-  <key>Umask</key>
-  <string>0002</string>
-  <key>ProgramArguments</key>
-  <array>
-    <string>{executablePath}</string>
-    <string>{progFile}</string>{additionalArgs}
-  </array>
-  <key>RunAtLoad</key>
-  <true/>
-  <key>KeepAlive</key>
-  <{keepAlive}/>
-  <key>WorkingDirectory</key>
-  <string>{progPath}</string>
-  <key>StandardErrorPath</key>
-  <string>{homePath}/Logs/{progName}/error.log</string>
-  <key>StandardOutPath</key>
-  <string>{homePath}/Logs/{progName}/output.log</string>
-  <key>EnvironmentVariables</key>
-  <dict>
-    <key>HOST</key>
-    <string>{host}</string>
-    <key>PrivateConfig</key>
-    <string>{PrivateConfig}</string>
-    <key>HOME</key>
-    <string>{homePath}</string>
-    <key>PATH</key>
-    <string>{PATH}</string>
-  </dict>
-</dict>
-</plist>
-'''
 myDomain = 'net.demayfamily'
 
 def main():
+    plistDict =  {  'Label': '{myDomain}.{progName}',
+                    'Umask': '0002',
+                    'ProgramArguments': ['{executablePath}', '{progFile}'],
+                    'RunAtLoad': True,
+                    'KeepAlive': True,
+                    'WorkingDirectory': '{progPath}',
+                    'StandardErrorPath': '{homePath}/Logs/{progName}/error.log',
+                    'StandardOutPath': '{homePath}/Logs/{progName}/output.log',
+                    'EnvironmentVariables': {}
+                }
+
     parser = argparse.ArgumentParser(description = 'Install user level agent and start it.',
         epilog='''The target program is expected to run indefinitely.''')
-    parser.add_argument("target", help="Target program")
+    parser.add_argument("target", help="Target program and command line arguments for the target.")
     parser.add_argument("--IAR", "--IARemove", dest="remove", action="store_true", help="Stops agent, unloads agent, deletes agent's plist file.  Other flags ignored.")
     parser.add_argument("--IAk", "--IAkeepAlive", dest="keepAlive", action="store_true", help="Sets KeepAlive flag in plist to true.")
+    parser.add_argument("--IAl", "--IAnotRunAtLoad", dest="runAtLoad", action="store_false", default=True, help="Sets RunAtLoad flag in plist to FALSE.")
     parser.add_argument("--IAW", "--IAdontWriteFile", dest="noWrite", action="store_true", help="Don't write the output file.")
     parser.add_argument("--IAX", "--IAdontStartAgent", dest="noStart", action="store_true", help="Don't invoke launchctl to load Agent.")
+    parser.add_argument("--IAa", "--IAadditions", dest="additions", action="store", help="Additional launchctl plist items encoded as JSON list.")
+    parser.add_argument("--IAe", "--IAenvironment", dest="environment", action="store", default='["HOST", "PrivateConfig", "HOME", "PATH"]',
+                        help="List of environment variables to copy to target environment.")
     allargs = parser.parse_known_args()
     args = allargs[0]
     clientArgs = allargs[1]
+    logger.debug('Client args are: %s'%clientArgs)
 
-    progFile = os.path.realpath(args.target)
-    logger.debug('Absolute path to target "%s"'%progFile)
-    progPath = os.path.dirname(progFile)
-    logger.debug('Absolute path of target directory "%s"'%progPath)
-    progName, ext = os.path.splitext(os.path.basename(progFile))
-    logger.debug('Program name is "%s", the extension is "%s"'%(progName, ext))
+    targetProgFile = os.path.realpath(args.target)
+    if not os.path.exists(targetProgFile):
+        logger.critical('The target file \"%s\" does not exist.'%targetProgFile)
+        return(1)
+    if os.stat(targetProgFile).st_mode & 0O100 == 0:
+        logger.critical('The target file \"%s\" is not executable.'%targetProgFile)
+        return(2)
+    targetProgPath = os.path.dirname(targetProgFile)
+    targetProgName, targetProgExt = os.path.splitext(os.path.basename(targetProgFile))
+    if targetProgExt == "":         # targetProgExt is empty, try first line of file to get kind
+        with open(targetProgFile) as fp:
+            l = fp.readline().rstrip()  # first line of file with trailing white space removed.
+            m = re.search(r'^#!.*?(\w+$)', l)    # if line starts with "#!" get last word on line.
+            if m is not None:
+                targetProgExt = m[1]
+                if 'python' in targetProgExt:
+                    targetProgExt = '.py'
+                elif 'sh' in targetProgExt:
+                    targetProgExt = '.sh'
+                else:
+                    targetProgExt = 'unknown'
+            ## leave targetProgExt as empty => bash
+            # else:
+            #     targetProgExt = 'unknown'
+
     homePath = os.path.expandvars('$HOME')
+    userName = getuser()
+    logger.debug('Absolute path to target "%s"'%targetProgFile)
+    logger.debug('Absolute path of target directory "%s"'%targetProgPath)
+    logger.debug('Program name is "%s", the extension is "%s"'%(targetProgName, targetProgExt))
     logger.debug('AbsolutePath of home directory is "%s"'%homePath)
+    plistDict['Label'] = myDomain + '.' + targetProgName
     plistFileName = os.path.join(homePath
         , 'Library'
         , 'LaunchAgents'
-        , '{myDomain}.{progName}.plist'.format(myDomain = myDomain, progName = progName))
+        , plistDict['Label'] + '.plist')
     logger.debug('Destination for plist file is: "%s"'%plistFileName)
 
+    ####   Remove option processing.
     if args.remove:
         if os.path.exists(plistFileName):
             logger.info('Stopping, removing Agent.')
@@ -148,41 +173,78 @@ def main():
                 logger.debug('Attempted removal of "%s" did not succeed.'%plistFileName)
                 pass
         else:
-            logger.info('Plist file "%s" does not exist; nothing to do.'%plistFileName)
-        return
-    if ext not in knownExecutables:
-        logger.warning("I don't know how to find an executable for extension: '%s'"%ext)
+            logger.info('Removing plist: plist file "%s" does not exist; nothing to do.'%plistFileName)
+        return(0)
+    ####    Finished removing
+
+    if targetProgExt not in knownExecutables:
+        logger.warning("I don't know how to find an executable for extension: '%s'"%targetProgExt)
         return 1
 
-    executablePath = knownExecutables[ext]()
+    executablePath = knownExecutables[targetProgExt]()
     logger.debug('The path to the executable is "%s"'%executablePath)
+    plistDict['ProgramArguments'][0] = executablePath
+    plistDict['ProgramArguments'][1] = targetProgFile
 
-    additionalArgs = ""
     for ca in clientArgs:
-        additionalArgs += '\n    <string>%s</string>'%ca
-    userName = getuser()
-    groupName = os.getgroups()[0]
-    keepAlive = 'true' if args.keepAlive else 'false'
-    host = os.environ['HOST']
-    PATH = os.environ['PATH']
+        # logger.debug('Processing client arg: %s'%ca)
+        plistDict['ProgramArguments'] += [ca]
+        # logger.debug('ProgramArguments item is now: %s'%plistDict['ProgramArguments'])
+
+    try:
+        if args.additions is not None:
+            additionalItems = json.loads(args.additions)
+            if not isinstance(additionalItems, list):
+                raise UserWarning('Additional items must be a list.')
+            for thisItem in additionalItems:
+                if not isinstance(thisItem, dict):
+                    raise UserWarning('Each additional item must be a dict.')
+                plistDict.update(thisItem)
+
+    except UserWarning as w:
+        logger.exception(w)
+
+    except Exception as e:
+        logger.critical('JSON for additional items is malformed.')
+        logger.exception(e)
+
+    logger.debug('Environment variables to copy to target environment (as str): %s'%args.environment)
+    try:
+        envVars = json.loads(args.environment)
+        logger.debug('Environment variables to copy to target environment: %s'%envVars)
+    except Exception as e:
+        logger.exception(e)
+        return(4)
+
+    for k in envVars:
+        plistDict['EnvironmentVariables'][k] = os.environ[k]
+
     # launchctl creates the StandardErrorPath and StandardOutPath as owned by root.
     # Then when the program starts and tries to write to the files, startup fails.
     #   If the directories already exist, launchctl leaves them alone.
-    os.makedirs('{homePath}/Logs/{progName}'.format(homePath=homePath, progName=progName), exist_ok=True)
-    PrivateConfig = os.environ['PrivateConfig']
-    plistContents = plistTemplate.format(myDomain = myDomain,
-                progName = progName,
-                userName = userName,
-                groupName = groupName,
-                progFile = progFile,
-                progPath = progPath,
-                executablePath = executablePath,
-                homePath = homePath,
-                additionalArgs = additionalArgs,
-                PATH = PATH,
-                host = host,
-                PrivateConfig = PrivateConfig,
-                keepAlive = keepAlive)
+    targetLogPath = os.path.join(homePath, 'Logs', targetProgName)
+    os.makedirs(targetLogPath, exist_ok=True)
+    plistDict['RunAtLoad'] = args.runAtLoad
+    plistDict['KeepAlive'] = args.keepAlive
+    plistDict['WorkingDirectory'] = targetProgPath
+    plistDict['StandardOutPath'] = os.path.join(targetLogPath, 'output.log')
+    plistDict['StandardErrorPath'] = os.path.join(targetLogPath, 'error.log')
+    plistContents = PL.dumps(plistDict, sort_keys = False).decode('UTF-8')
+    # plistContents = plistTemplate.format(myDomain = myDomain,
+    #             progName = targetProgName,
+    #             userName = userName,
+    #             groupName = groupName,
+    #             progFile = targetProgFile,
+    #             progPath = targetProgPath,
+    #             executablePath = executablePath,
+    #             homePath = homePath,
+    #             additionalArgs = additionalArgs,
+    #             RunAtLoad = 'true' if args.RunAtLoad else 'false',
+    #             additionalItems = additionalItems,
+    #             PATH = PATH,
+    #             host = host,
+    #             PrivateConfig = PrivateConfig,
+    #             keepAlive = 'true' if args.keepAlive else 'false')
     logger.debug('Contents of plist file are to be:\n%s'%plistContents)
     if args.noWrite:
         logger.info('Plist file NOT written to LanchAgents NOR started.')
